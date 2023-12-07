aec30dc5aa2699fa1f48f411f7016aab14ee3923
Belt0M <belongtomars@gmail.com>
2023-12-07 18:47:15 +0100

    Change login submit navigate

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,44 +1,54 @@
-import React,{ FC } from 'react';
-import { FaUser,FaLock } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import React, { FC, FormEvent } from 'react'
+import { FaLock, FaUser } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
 
 const LoginPage: FC = () => {
+	const navigate = useNavigate()
 
-    React.useEffect(() => {
-        document.body.classList.add('centered-page-body');
-        return () => {
-          document.body.classList.remove('centered-page-body');
-        };
-      }, []);
+	React.useEffect(() => {
+		document.body.classList.add('centered-page-body')
+		return () => {
+			document.body.classList.remove('centered-page-body')
+		}
+	}, [])
 
-  return (
-    <div className='wrapper'>
-      <form action="">
-        <h1>Login</h1>
-        <div className='input-box'>
-          <input type="text" placeholder='Username' required></input>
-          <FaUser className='icon' />
-        </div>
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault()
+		alert('f')
+		navigate('/')
+	}
 
-        <div className='input-box'>
-          <input type="password" placeholder='Password' required></input>
-          <FaLock className='icon'/>
-        </div>
+	return (
+		<div className='wrapper'>
+			<form action='' onSubmit={handleSubmit}>
+				<h1>Login</h1>
+				<div className='input-box'>
+					<input type='text' placeholder='Username' required></input>
+					<FaUser className='icon' />
+				</div>
 
-        <div className="remember-forgot">
-          <label><input type='checkbox'/>Remember me</label>
-          <a href='#'>Forgot password?</a>
-        </div>
-        <Link to='/'>
-        <button type="submit" >Login</button>
-        </Link>
+				<div className='input-box'>
+					<input type='password' placeholder='Password' required></input>
+					<FaLock className='icon' />
+				</div>
 
-        <div className="register-link">
-            <p>Don't have an account? <Link to ='/register'>Register</Link></p>
-          </div>       
-      </form>
-    </div>
-  );
-};
+				<div className='remember-forgot'>
+					<label>
+						<input type='checkbox' />
+						Remember me
+					</label>
+					<a href='#'>Forgot password?</a>
+				</div>
+				<button type='submit'>Login</button>
 
-export default LoginPage;
+				<div className='register-link'>
+					<p>
+						Don't have an account? <Link to='/register'>Register</Link>
+					</p>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default LoginPage
